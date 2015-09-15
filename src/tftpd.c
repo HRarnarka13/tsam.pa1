64271a68cb5da5ea3798dfcf34e4c6888a624575
arnarka13 <arnarka13@skel.ru.is>
2015-09-15 10:33:13 +0000

    setup

---
+++ src/tftpd.c
@@ -0,0 +1,103 @@
+/* A UDP echo server with timeouts.
+ *  *
+ *   * Note that you will not need to use select and the timeout for a
+ *    * tftp server. However, select is also useful if you want to receive
+ *     * from multiple sockets at the same time. Read the documentation for
+ *      * select on how to do this (Hint: Iterate with FD_ISSET()).
+ *       */
+
+#include <assert.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int getOpcode(char* packet){
+	return packet[1];
+}
+
+void getFileName(char* packet, char* fileName) {
+	strcpy(fileName, packet + 2);
+}
+
+void getMode(char* packet, char*fileName, char* mode) {
+	int fileNameLength = 0;
+	fileNameLength = strlen(fileName); // get the length of the file name
+	strcpy(mode, packet + 2 + fileNameLength + 1); // get the mode from the packet
+}
+
+int main(int argc, char **argv)
+{
+        int sockfd;
+        struct sockaddr_in server, client;
+        char message[512];
+
+        /* Create and bind a UDP socket */
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        memset(&server, 0, sizeof(server));
+        server.sin_family = AF_INET;
+        /* Network functions need arguments in network byte order instead of
+ 	*  host byte order. The macros htonl, htons convert the values, */
+        server.sin_addr.s_addr = htonl(INADDR_ANY);
+        server.sin_port = htons(atoi(argv[1]));
+        bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+        for (;;) {
+                fd_set rfds;
+                struct timeval tv;
+                int retval;
+
+                /* Check whether there is data on the socket fd. */
+                FD_ZERO(&rfds);
+                FD_SET(sockfd, &rfds);
+
+                /* Wait for five seconds. */
+                tv.tv_sec = 5;
+                tv.tv_usec = 0;
+                retval = select(sockfd + 1, &rfds, NULL, NULL, &tv);
+
+                if (retval == -1) {
+                        perror("select()");
+                } else if (retval > 0) {
+                        /* Data is available, receive it. */
+                        assert(FD_ISSET(sockfd, &rfds));
+
+                        /* Copy to len, since recvfrom may change it. */
+                        socklen_t len = (socklen_t) sizeof(client);
+                        /* Receive one byte less than declared,
+ 			 * because it will be zero-termianted
+ 			 * below. */
+                        ssize_t n = recvfrom(sockfd, message,
+                                             sizeof(message) - 1, 0,
+                                             (struct sockaddr *) &client,
+                                             &len);			
+                        /* Send the message back. */
+                        sendto(sockfd, message, (size_t) n, 0,
+                               (struct sockaddr *) &client,
+                               (socklen_t) sizeof(client));
+                        /* Zero terminate the message, otherwise
+ 			 * printf may access memory outside of the
+ 			 * string. */
+                        message[n] = '\0';
+
+			int opCode = getOpcode(message);
+			char fileName[512];
+			getFileName(message, fileName);
+			char mode[100];
+			getMode(message, fileName, mode);
+			fprintf(stdout, "opcode : %d\n", opCode);
+			fprintf(stdout, "fileName: %s\n", fileName);
+			fprintf(stdout, "mode : %s\n", mode);
+                        /* Print the message to stdout and flush. */
+                        // fprintf(stdout, "Received:\n%s\n", message);
+                        fflush(stdout);
+                } else {
+                        fprintf(stdout, "No message in five seconds.\n");
+                        fflush(stdout);
+                }
+        }
+}
