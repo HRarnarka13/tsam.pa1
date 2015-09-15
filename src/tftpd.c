3ef55e33d7d38a15ae60a5bc3fbd62d07388ddb2
gunnark13 <gunnark13@skel.ru.is>
2015-09-15 16:22:59 +0000

    packet printing

--- src/tftpd.c
+++ src/tftpd.c
@@ -30,6 +30,34 @@ void getMode(char* packet, char*fileName, char* mode) {
 	strcpy(mode, packet + 2 + fileNameLength + 1); // get the mode from the packet
 }
 
+void readChunk(char* fileName){
+    fprintf(stdout, "test!! \n");
+    fflush(stdout);
+    FILE* file = NULL;
+    char path[512];
+    strcpy(path, "data/");
+    strcat(path, fileName);
+    file = fopen(path, "r");
+    char chunk[516];
+    //unsigned short blockNumber = 1;
+    if(file != NULL){
+        size_t i;
+        while (!feof(file)){
+            i = fread(chunk, 1, 512, file);
+            /*if(i < 512){
+                //
+            }*/
+            fprintf(stdout, "Size of i: %zu\n", i);
+            //fprintf(stdout, "%s", chunk);
+        }
+
+        //memset(&chunk,0,sizeof(chunk));       
+        fclose(file);
+    }else{
+        perror("Problem with reading file");
+    }
+}
+
 int main(int argc, char **argv)
 {
         int sockfd;
@@ -69,8 +97,8 @@ int main(int argc, char **argv)
                         /* Copy to len, since recvfrom may change it. */
                         socklen_t len = (socklen_t) sizeof(client);
                         /* Receive one byte less than declared,
- 			 * because it will be zero-termianted
- 			 * below. */
+ 			            * because it will be zero-termianted
+ 			            * below. */
                         ssize_t n = recvfrom(sockfd, message,
                                              sizeof(message) - 1, 0,
                                              (struct sockaddr *) &client,
@@ -80,21 +108,32 @@ int main(int argc, char **argv)
                                (struct sockaddr *) &client,
                                (socklen_t) sizeof(client));
                         /* Zero terminate the message, otherwise
- 			 * printf may access memory outside of the
- 			 * string. */
+ 			            * printf may access memory outside of the
+ 			            * string. */
                         message[n] = '\0';
 
-			int opCode = getOpcode(message);
-			char fileName[512];
-			getFileName(message, fileName);
-			char mode[100];
-			getMode(message, fileName, mode);
-			fprintf(stdout, "opcode : %d\n", opCode);
-			fprintf(stdout, "fileName: %s\n", fileName);
-			fprintf(stdout, "mode : %s\n", mode);
+			            int opCode = getOpcode(message);
+			            char fileName[512];
+			            getFileName(message, fileName);
+			            char mode[100];
+			            getMode(message, fileName, mode);
+			            fprintf(stdout, "opcode : %d\n", opCode);
+			            fprintf(stdout, "fileName: %s\n", fileName);
+			            fprintf(stdout, "mode : %s\n", mode);
                         /* Print the message to stdout and flush. */
                         // fprintf(stdout, "Received:\n%s\n", message);
                         fflush(stdout);
+                        readChunk(fileName);
+                        /*FILE* file = NULL;
+                        char chunk[512];
+                        memset(chunk,0,sizeof(chunk));
+                        
+                        file = fopen(fileName, "r");
+                        fread(message, 512, byteNumber, file);
+                        fclose(file);
+
+                        fprintf(stdout, "Chunk: %s\n", message);
+                        fflush(stdout);*/
                 } else {
                         fprintf(stdout, "No message in five seconds.\n");
                         fflush(stdout);
